67ae0abc7fdc12466113e8d3d62ec8fd8fb66270
mhahn <mhahn@student.42heilbronn.de>
2021-07-20 21:29:07 +0200

    Reading in main.c works now

--- src/native/main.c
+++ src/native/main.c
@@ -5,18 +5,24 @@
 #include "gnl-server-test.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
 	if (argc == 3) {
 		int fd = establishConnection(atoi(argv[1]), argv[2]);
 		printf("%d\n", fd);
 		if (fd != -1) {
-			char *line;
-			while (1) {
+			char line[10];
+			/*while (1) {
 				line = get_next_line(fd);
 				if (line != NULL) {
 					printf("%s", line);
 				}
+			}*/
+			int ret = read(fd, line, 10);
+			while (ret != -1) {
+				printf("%d | %s\n", ret, line);
+				ret = read(fd, line, 10);
 			}
 		}
 	} else {
